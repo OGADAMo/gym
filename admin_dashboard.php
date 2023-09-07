a2f0853649189718ec72501f4f7c12a283447fb6
OGADAMo <adam.marjanovic2@gmail.com>
2023-09-07 15:21:25 +0200

    upload_photo

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -58,6 +58,31 @@ if (!isset($_SESSION['admin_id'])) {
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+
+    <script>
+        Dropzone.options.dropzoneUpload = {
+            url: "upload_photo.php",
+            paramName: "photo",
+            maxFileSize: 20,
+            acceptedFiles: "image/*",
+            init: function() {
+                this.on("success", function(file, response) {
+                    // File upload success
+                    const jsonResponse =JSON.parse(response):
+
+                    if(jsonResponse.success){
+                        document.querySelector("#photoPathInput").value =jsonResponse.photo_pathM
+                    } else {
+                        console.error(jsonResponse.error);
+                    }
+                });
+
+               
+            };
+
+        }
+    </script>
 </body>
 </html>
